cb7932f6582bf1c6d1c04eed2541b3ff1d3ae346
deyangye <deyangye@ziguangcn.com>
2020-01-14 13:36:10 +0800

    MERGE MASTER

--- src/main/java/threadcoreknowledge/uncaughtexception/MyUncaughtExceptionHandler.java
+++ src/main/java/threadcoreknowledge/uncaughtexception/MyUncaughtExceptionHandler.java
@@ -13,6 +13,6 @@ public class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandl
         Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
 
         ParentThreadCannotStopWithChildThreadException.runChildThread();
-        System.out.println("MASTER BRANCH");
+        System.out.println("MERGE MASTER BRANCH");
     }
 }
